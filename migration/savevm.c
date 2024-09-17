24f190e5e8396cd8d604b46c6e9ead6817c027b8
Fabiano Rosas <farosas@suse.de>
2024-09-17 15:58:02 -0300

    migration/multifd: Fix rb->receivedmap cleanup race
    commit 4ce56229087860805877075ddb29dd44578365a9 upstream.
    
    Fix a segmentation fault in multifd when rb->receivedmap is cleared
    too early.
    
    After commit 5ef7e26bdb ("migration/multifd: solve zero page causing
    multiple page faults"), multifd started using the rb->receivedmap
    bitmap, which belongs to ram.c and is initialized and *freed* from the
    ram SaveVMHandlers.
    
    Multifd threads are live until migration_incoming_state_destroy(),
    which is called after qemu_loadvm_state_cleanup(), leading to a crash
    when accessing rb->receivedmap.
    
    process_incoming_migration_co()        ...
      qemu_loadvm_state()                  multifd_nocomp_recv()
        qemu_loadvm_state_cleanup()          ramblock_recv_bitmap_set_offset()
          rb->receivedmap = NULL               set_bit_atomic(..., rb->receivedmap)
      ...
      migration_incoming_state_destroy()
        multifd_recv_cleanup()
          multifd_recv_terminate_threads(NULL)
    
    Move the loadvm cleanup into migration_incoming_state_destroy(), after
    multifd_recv_cleanup() to ensure multifd threads have already exited
    when rb->receivedmap is cleared.
    
    Adjust the postcopy listen thread comment to indicate that we still
    want to skip the cpu synchronization.
    
    Intel-SIG: commit 4ce562290878 migration/multifd: Fix rb->receivedmap cleanup race
    
    CC: qemu-stable@nongnu.org
    Fixes: 5ef7e26bdb ("migration/multifd: solve zero page causing multiple page faults")
    Signed-off-by: Fabiano Rosas <farosas@suse.de>
    Link: https://lore.kernel.org/r/20240917185802.15619-3-farosas@suse.de
    [peterx: added comment in migration_incoming_state_destroy()]
    Signed-off-by: Peter Xu <peterx@redhat.com>
    
     Conflicts:
    	migration/migration.c
    [jz: resolve context conflict]
    Signed-off-by: Jason Zeng <jason.zeng@intel.com>

--- migration/savevm.c
+++ migration/savevm.c
@@ -2960,7 +2960,10 @@ int qemu_loadvm_state(QEMUFile *f)
     trace_qemu_loadvm_state_post_main(ret);
 
     if (mis->have_listen_thread) {
-        /* Listen thread still going, can't clean up yet */
+        /*
+         * Postcopy listen thread still going, don't synchronize the
+         * cpus yet.
+         */
         return ret;
     }
 
@@ -3003,7 +3006,6 @@ int qemu_loadvm_state(QEMUFile *f)
         }
     }
 
-    qemu_loadvm_state_cleanup();
     cpu_synchronize_all_post_init();
 
     return ret;

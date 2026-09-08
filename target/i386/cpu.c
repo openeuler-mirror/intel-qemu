openeuler-mirror/qemu

commit 3657cb5a6ac687e0c59f647cfabdba791d7dd777

target/i386: Add tsc-adjust to Hygon Chengdu‑v2
Hygon Chengdu‑v2 extends the original Chengdu model by adding SM3/SM4 and
corresponds to the Hygon 4H (7447v) CPU family. The 4H CPUs expose the
tsc-adjust feature in CPUID leaf 0x7, so the Chengdu‑v2 model should
advertise this bit to guests as well.

Expose tsc-adjust in the Chengdu‑v2 model to match the behavior of real 4H
hardware.

Hygon-SIG: commit none hygon target/i386: Enable tsc-adjust CPUID bit for
Hygon Chengdu‑v2

Signed-off-by: Wei Wang <wangwww@hygon.cn>
Tested-by: Yongwei Xu <xuyongwei@hygon.cn>

target/i386/cpu.c
@@ -6032,6 +6032,7 @@ static const X86CPUDefinition builtin_x86_defs[] = {
             {
               .version = 2,
               .props = (PropValue[]) {
+                  { "tsc-adjust", "on" },
                   { "hygon-sm3", "on" },
                   { "hygon-sm4", "on" },
                   { "model-id",

openeuler-mirror/intel-qemu

commit 8b6b74403734c199f53699b07c7c7b5052d5a83a

target/i386: Move tsc_adjust from Dharma to Chengdu
A rebase error mistakenly applied the tsc_adjust flag to the Hygon Dharma
CPU model. Move it to the Chengdu model.

Fixes: 3657cb5a6ac6 ("target/i386: Add tsc-adjust to Hygon Chengdu-v2")
Signed-off-by: Wei Wang <wangwww@hygon.cn>

target/i386/cpu.c
@@ -6009,7 +6009,6 @@ static const X86CPUDefinition builtin_x86_defs[] = {
             {
               .version = 2,
               .props = (PropValue[]) {
-                  { "tsc-adjust", "on" },
                   { "hygon-sm3", "on" },
                   { "hygon-sm4", "on" },
                   { "movu", "on" },
@@ -6089,6 +6088,7 @@ static const X86CPUDefinition builtin_x86_defs[] = {
               .version = 2,
               .props = (PropValue[]) {
                   { "avx512-vp2intersect", "on"},
+                  { "tsc-adjust", "on" },
                   { "hygon-sm3", "on" },
                   { "hygon-sm4", "on" },
                   { "movu", "on" },